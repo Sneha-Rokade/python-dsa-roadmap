703920195f7fe5e8de56e42d610bda0011d959c1
Sneha-Rokade <bioblister@gmail.com>
2026-07-02 17:34:06 +0530

    Solved Exercise 7th

---
+++ Python-Excercises/Exercise-7.py
@@ -0,0 +1,28 @@
+# Exercise 7. List Manipulation: Add and Remove
+# Practice Problem: Create a list of 5 fruits. Add a new fruit to the end of the list, then remove the second fruit (at index 1).
+
+# Exercise Purpose: This exercise teaches “Dynamic Collection Management.” Lists are rarely static; being able to modify, expand, and prune them is essential for handling data like shopping carts, user lists, or inventory systems.
+
+# Given Input: fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+# Expected Output: ['apple', 'cherry', 'date', 'elderberry', 'fig']
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+print("original list : ", fruits )
+
+fruits.pop(1)
+
+print("new list : ", fruits )
+
+fruits.remove("date")
+
+print("new list : ", fruits )
+
+del fruits[0]
+
+print("new list : ", fruits )
+
+fruits.clear()
+
+print("new list : ", fruits )
